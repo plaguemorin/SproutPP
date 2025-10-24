00a878915dee9f1026801420f6bf687c8097a733
Philippe Lague-Morin <plaguemorin@users.noreply.github.com>
2025-10-24 00:00:13 -0400

    Replaced possible infinite recursion with possible infinite loop

--- src/compositing_property_resolver.cpp
+++ src/compositing_property_resolver.cpp
@@ -12,38 +12,56 @@ void CompositingPropertyResolver::registerPropertySourceFactory(std::string_view
   _property_source_factory.try_emplace(std::hash<std::string_view>{}(sourceName), factory);
 }
 
-void CompositingPropertyResolver::registerPropertySource(std::unique_ptr<PropertySource> &&source) {
-  std::string postImport;
+std::string CompositingPropertyResolver::extractImportConfig(const PropertySource &source) const {
+  // Does this source have import lines?
+  // Import should be in the form of, <sourceName>:<sourceParam>
+  // Like file:/etc/config.properties
+  // Or zookeeper:localhost:1234/config/rootName
+  const auto importConfig = source.getProperty("import.config");
+  return std::holds_alternative<std::string>(importConfig)
+             ? std::get<std::string>(importConfig)
+             : std::string();
+}
 
-  if (source->isStatic() && !source->hasValues()) {
+void CompositingPropertyResolver::handleDynamicSourceNotifications(PropertySource &source) {
+  // if this is a static source (the values cannot change)
+  if (!source.isStatic()) {
+    // Make sure we get notified of values changes so we can broadcast it
+    source.clearValueWatchers();
+    source.addValueWatcher([this](std::string_view propertyName) {
+      valueChanged(propertyName);
+    });
+  }
+}
+
+std::unique_ptr<PropertySource> CompositingPropertyResolver::loadPropertySourceFromImportString(std::string_view postImport) {
+  // Do we have another source to load?
+  if (const auto delim = postImport.find(':');
+      delim != std::string::npos) {
+    const auto sourceName = postImport.substr(0, delim);
+    const auto sourceParam = postImport.substr(delim + 1);
+
+    // Was this sourceName registered?
+    if (const auto it = _property_source_factory.find(std::hash<std::string_view>{}(sourceName));
+        it != _property_source_factory.end()) {
+      return it->second(sourceParam);
+    }
+  }
+
+  return nullptr;
+}
+void CompositingPropertyResolver::registerPropertySource(std::unique_ptr<PropertySource> &&source) {
+  if (!source || (source->isStatic() && !source->hasValues())) {
     // If the source is static and has no values, don't bother doing
     // anything and just return silently
     return;
   }
 
-  // Does this source have import lines?
-  if (const auto importConfig = source->getProperty("import.config");
-      std::holds_alternative<std::string>(importConfig)) {
-    // Import should be in the form of, <sourceName>:<sourceParam>
-    // Like file:/etc/config.properties
-    // Or zookeeper:localhost:1234/config/rootName
-    postImport = std::get<std::string>(importConfig);
-  }
-
-  // if this is a static source (the values cannot change)
-  if (!source->isStatic()) {
-    // Make sure we get notified of values changes so we can broadcast it
-    source->clearValueWatchers();
-    source->addValueWatcher([this](std::string_view propertyName) {
-      valueChanged(propertyName);
-    });
-  }
-
-  _sources.emplace_back(std::move(source));
-
-  // Do we have a post-import ?
-  if (!postImport.empty()) {
-    loadPropertySource(postImport);
+  while (source) {
+    const auto postImport = extractImportConfig(*source);
+    handleDynamicSourceNotifications(*source);
+    _sources.emplace_back(std::move(source));
+    source = loadPropertySourceFromImportString(postImport);
   }
 }
 
@@ -57,23 +75,6 @@ PropertySource::Value CompositingPropertyResolver::getProperty(std::string_view 
   }
 
   return std::monostate();
-}
-
-std::error_code CompositingPropertyResolver::loadPropertySource(std::string_view propertySourceName) {
-  // Find the first ":" to split on
-  if (const auto delim = propertySourceName.find(':');
-      delim != std::string::npos) {
-    const auto sourceName = propertySourceName.substr(0, delim);
-    const auto sourceParam = propertySourceName.substr(delim + 1);
-
-    // Was this sourceName registered?
-    if (const auto it = _property_source_factory.find(std::hash<std::string_view>{}(sourceName));
-        it != _property_source_factory.end()) {
-      registerPropertySource(it->second(sourceParam));
-    }
-  }
-
-  return {};
 }
 
 }// namespace framework::impl
--- src/compositing_property_resolver.h
+++ src/compositing_property_resolver.h
@@ -10,25 +10,27 @@
 
 namespace framework::impl {
 
-  /**
+/**
    * Class CompositingPropertyResolver
    */
-  class CompositingPropertyResolver : public virtual PropertyResolver {
-    std::map<std::string, PropertySource::Value> _staticSources;
-    std::list<std::unique_ptr<PropertySource>> _sources;
-    std::map<size_t, PropertySourceFactory> _property_source_factory;
+class CompositingPropertyResolver : public virtual PropertyResolver {
+  std::map<std::string, PropertySource::Value, std::less<>> _staticSources;
+  std::list<std::unique_ptr<PropertySource>> _sources;
+  std::map<size_t, PropertySourceFactory> _property_source_factory;
 
-    void valueChanged(std::string_view propertyName);
+  void valueChanged(std::string_view propertyName);
+  std::string extractImportConfig(const PropertySource &source) const;
+  void handleDynamicSourceNotifications(PropertySource &source);
+  std::unique_ptr<PropertySource> loadPropertySourceFromImportString(std::string_view postImport);
 
-  protected:
-    void registerPropertySourceFactory(std::string_view sourceName, PropertySourceFactory factory) override;
+protected:
+  void registerPropertySourceFactory(std::string_view sourceName, PropertySourceFactory factory) override;
 
-  public:
-    ~CompositingPropertyResolver() override = default;
+public:
+  ~CompositingPropertyResolver() override = default;
 
-    void registerPropertySource(std::unique_ptr<PropertySource> &&source) override;
-    PropertySource::Value getProperty(std::string_view propertyName) override;
-    std::error_code loadPropertySource(std::string_view propertySourceName);
-  };
+  void registerPropertySource(std::unique_ptr<PropertySource> &&source) override;
+  PropertySource::Value getProperty(std::string_view propertyName) override;
+};
 
-} // namespace framework::impl
+}// namespace framework::impl
