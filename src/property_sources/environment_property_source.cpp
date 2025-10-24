7ad4cddba9c0fc2e152e1e93347baa4efa23c196
Philippe Lague-Morin <plaguemorin@users.noreply.github.com>
2025-10-24 01:13:04 -0400

    Make MSVC happy

--- src/property_sources/environment_property_source.cpp
+++ src/property_sources/environment_property_source.cpp
@@ -2,6 +2,10 @@
 #include "environment_property_source.h"
 
 #include <cstdlib>
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#endif
 
 namespace {
 std::string makeName(std::string_view name) {
@@ -28,15 +32,31 @@ std::string makeName(std::string_view name) {
 namespace framework::impl {
 bool EnvironmentPropertySource::containsProperty(std::string_view propertyName) const {
   const auto name = makeName(propertyName);
+#ifdef _WIN32
+  size_t requiredSize;
+  getenv_s(&requiredSize, nullptr, 0, name.c_str());
+  return requiredSize > 0;
+#else
   return std::getenv(name.c_str()) != nullptr;
+#endif
 }
 
 PropertySource::Value EnvironmentPropertySource::getProperty(std::string_view propertyName) const {
   const auto name = makeName(propertyName);
 
+#ifdef _WIN32
+  size_t requiredSize;
+  getenv_s(&requiredSize, nullptr, 0, name.c_str());
+  if (requiredSize > 0) {
+    std::string result(requiredSize, '\0');
+    getenv_s(&requiredSize, result.data(), result.size(), name.c_str());
+    return result;
+  }
+#else
   if (const auto rc = std::getenv(name.c_str())) {
     return std::string(rc);
   }
+#endif
 
   return std::monostate{};
 }
